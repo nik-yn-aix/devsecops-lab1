3df49110413f4ccfe36c538d66f6554b9d110b2b
nik-yn-aix <ritdump@gmail.com>
2026-02-16 12:08:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-const apiKey = process.env.WEATHER_API_KEY;
-if (!apiKey) {
-  console.error("CRITICAL ERROR: No API Key found!");
-  process.exit(1);
-}
-console.log("App is running securely.");
+console.log("Unit tests are running...");
